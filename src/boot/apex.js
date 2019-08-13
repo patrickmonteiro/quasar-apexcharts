3bc6dac81e253e164a772bf5acdbf5e2127cd3da
Patrick Monteiro <eng.patrickmonteiro@gmail.com>
2019-08-12 21:20:41 -0300

    First commit :tada:

---
+++ src/boot/apex.js
@@ -0,0 +1,9 @@
+import Vue from 'vue'
+import VueApexCharts from 'vue-apexcharts'
+
+Vue.component('apexchart', VueApexCharts)
+
+// export default async ({ Vue }) => {
+// Vue.prototype.$apex = ApexCharts
+// Vue.use(ApexCharts)
+// }
