2fb8b1d1b5b3970e69d1595c70bf5a2284e9bbe3
Patrick Monteiro <eng.patrickmonteiro@gmail.com>
2021-01-21 23:40:15 -0300

    remove cardbase for upgrade performance

--- src/components/ApexLineColumn.vue
+++ src/components/ApexLineColumn.vue
@@ -1,16 +1,10 @@
 <template>
-  <card-base>
-    <apexchart type="line" height="230px" :options="chartOptions" :series="series" />
-  </card-base>
+  <apexchart type="line" height="230px" :options="chartOptions" :series="series" />
 </template>
 
 <script>
-import CardBase from 'components/CardBase'
 export default {
   name: 'ApexLineColumn',
-  components: {
-    CardBase
-  },
   data () {
     return {
       series: [{
