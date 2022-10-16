1e6d87f29e69abb97cb0a6c8d3816d0a68dcb1e5
DaywisonSilva <daywisonsilvasou@gmail.com>
2022-10-16 22:08:24 +0000

    feat: add new ApexHeatmap chart

---
+++ src/components/charts/ApexHeatmap.vue
@@ -0,0 +1,114 @@
+<template>
+  <apexchart
+    height='300'
+    type='heatmap'
+    :options='options'
+    :series='series'
+  ></apexchart>
+</template>
+
+<script>
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'ApexHeatmap',
+  data () {
+    return {
+      options: {
+        chart: {
+          height: 350,
+          type: 'heatmap'
+        },
+        dataLabels: {
+          enabled: false
+        },
+        colors: ['#008FFB'],
+        title: {
+          text: 'HeatMap Chart (Single color)'
+        }
+      },
+      series: [
+        {
+          name: 'Metric1',
+          data: this.generateData(18, {
+            min: 0,
+            max: 90
+          })
+        },
+        {
+          name: 'Metric2',
+          data: this.generateData(18, {
+            min: 0,
+            max: 90
+          })
+        },
+        {
+          name: 'Metric3',
+          data: this.generateData(18, {
+            min: 0,
+            max: 90
+          })
+        },
+        {
+          name: 'Metric4',
+          data: this.generateData(18, {
+            min: 0,
+            max: 90
+          })
+        },
+        {
+          name: 'Metric5',
+          data: this.generateData(18, {
+            min: 0,
+            max: 90
+          })
+        },
+        {
+          name: 'Metric6',
+          data: this.generateData(18, {
+            min: 0,
+            max: 90
+          })
+        },
+        {
+          name: 'Metric7',
+          data: this.generateData(18, {
+            min: 0,
+            max: 90
+          })
+        },
+        {
+          name: 'Metric8',
+          data: this.generateData(18, {
+            min: 0,
+            max: 90
+          })
+        },
+        {
+          name: 'Metric9',
+          data: this.generateData(18, {
+            min: 0,
+            max: 90
+          })
+        }
+      ]
+    }
+  },
+  methods: {
+    generateData (count, yrange) {
+      let i = 0
+      const series = []
+      while (i < count) {
+        const x = (i + 1).toString()
+        const y = Math.floor(Math.random() * (yrange.max - yrange.min + 1)) + yrange.min
+        series.push({
+          x: x,
+          y: y
+        })
+        i++
+      }
+      return series
+    }
+  }
+})
+</script>
